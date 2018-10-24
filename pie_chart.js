18e96285e1d45827cea8ef887f08c0bc92cb6306
ferdinandkff <ferdinand.krumhoff@yahoo.de>
2018-10-24 18:11:53 +0200

    Add nationality pie chart

--- pie_chart.js
+++ pie_chart.js
@@ -1,9 +1,10 @@
 // Load Google charts
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawGenderChart);
+google.charts.setOnLoadCallback(drawCountryChart);
 
-// Draw chart for nationalities
-function drawChart() {
+// Draw chart for genders
+function drawGenderChart() {
 
   let genderArr = [];
 
@@ -34,8 +35,42 @@ function drawChart() {
     title: 'This is the gender ratio of your year:'
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  var chart = new google.visualization.PieChart(document.getElementById('genderChart'));
 
   chart.draw(data, options);
 }
 
+function drawCountryChart() {
+
+  let countryArr = [];
+
+  for(let i = 0; i < localStorage.length; i++) {
+    let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    countryArr.push(usr.nationality);
+  }
+  
+  // Create object listing the number of times each country occurs
+  var numberOfCountry = {};
+
+  countryArr.forEach(function(n) {  // Creates a property for each country with its amount as the value
+    numberOfCountry[n] = (numberOfCountry[n] || 0) + 1; 
+  });
+
+  var rankedCntrs = []; // Create 2-dimensional array from object numberOfCountry
+  for(let x in numberOfCountry){
+    rankedCntrs.push([x, numberOfCountry[x]])
+  }
+
+  rankedCntrs.sort(function(a,b){ // Sort array descending by the amount a country occurs
+    return b[1] - a[1];
+  }).unshift(["Country", "Number"]); // Add labels
+
+  var data = google.visualization.arrayToDataTable(rankedCntrs); // Make pie chart of nationalities with the array rankedCntrs
+
+  var options = {
+    title: 'This is where your fellow students are from:'
+  };
+  
+  var chart = new google.visualization.PieChart(document.getElementById('countryChart'));
+  chart.draw(data, options);
+}
