3b0bbc622c6bbe557fd48f0ae663d7d3025bac0a
ferdinandkff <ferdinand.krumhoff@yahoo.de>
2018-10-24 11:30:36 +0200

    Display gender ratio in pie chart

--- pie_chart.js
+++ pie_chart.js
@@ -5,8 +5,24 @@ google.charts.setOnLoadCallback(drawChart);
 // Draw chart for nationalities
 function drawChart() {
 
-  let nrFemales = 60; // Insert retrieve method here
-  let nrMales = 60;
+  let genderArr = [];
+
+  for(let i=0; i<localStorage.length; i++){
+    let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    genderArr.push(usr.gender);
+  }
+
+  let nrFemales = 0;
+  let nrMales = 0;
+
+  for(let i=0; i<genderArr.length; i++){
+    
+    if(genderArr[i] == "female") {
+      nrFemales += 1;
+    }else if(genderArr[i] == "male") {
+      nrMales += 1;
+    }
+  }
 
   var data = google.visualization.arrayToDataTable([
     ['Gender', 'Number'],
