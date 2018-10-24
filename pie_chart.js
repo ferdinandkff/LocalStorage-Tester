23acf7e1ae9affc15fed3de372100120ad184f38
ferdinandkff <ferdinand.krumhoff@yahoo.de>
2018-10-24 10:36:04 +0200

    create link between pages. Add gender variables

--- pie_chart.js
+++ pie_chart.js
@@ -5,18 +5,17 @@ google.charts.setOnLoadCallback(drawChart);
 // Draw chart for nationalities
 function drawChart() {
 
-    var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work', 8],
-    ['Friends', 2],
-    ['Eat', 2],
-    ['TV', 2],
-    ['Gym', 2],
-    ['Sleep', 8]
+  let nrFemales = 60; // Insert retrieve method here
+  let nrMales = 60;
+
+  var data = google.visualization.arrayToDataTable([
+    ['Gender', 'Number'],
+    ['Female', nrFemales],
+    ['Male', nrMales,
   ]);
 
   var options = {
-    title: 'This is where your fellow students are from:'
+    title: 'This is the gender ratio of your year:'
   };
 
   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
