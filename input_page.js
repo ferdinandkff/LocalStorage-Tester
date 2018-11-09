3318f7b686dcb5f8b17aee2331967b2f2fd29325
ferdinandkff <ferdinand.krumhoff@yahoo.de>
2018-11-09 15:38:35 +0100

    One class to rule them all: ChartData can take in any number of answer possibilities and creates both pie charts and bar charts

--- input_page.js
+++ input_page.js
@@ -28,11 +28,116 @@ function saveInput() { // Save Input in local storage
     localStorage.setItem(id, JSON.stringify(user)); // Store object as string with JSON
 }
 
-function resetAnswers() { //Resets the radio buttons
-    var answer = document.getElementsByName("gender");
-    
-    for(var i=0;i<answer.length;i++) {
-        answer[i].checked = false;
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCharts);
+
+class ChartData {
+
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
     }
 
+    drawPieChart() {
+        let valueArr = [];
+
+        for (let i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i) != "Users") {
+                let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                valueArr.push(usr[this.name]);
+            }
+        }
+
+        // Create object listing the number of times each country occurs
+        var numberOfValue = {};
+
+        valueArr.forEach(function(n) {  // Creates a property for each country with its amount as the value
+            numberOfValue[n] = (numberOfValue[n] || 0) + 1; 
+        });
+
+        var rankedValues = []; // Create 2-dimensional array from object numberOfCountry
+        
+        for(let x in numberOfValue){
+            rankedValues.push([x, numberOfValue[x]])
+        }
+
+        rankedValues.sort(function(a,b){ // Sort array descending by the amount a country occurs
+            return b[1] - a[1];
+        }); 
+
+        // We just want to display the top 5 recurring countries in the dataset, therefore we will further adjust rankedCntrs
+        var otherValues = 0; // Empty variable for the total amount of countries outside the top five
+
+        for(let i=5; i<rankedValues.length; i++) { // Start from 5 and loop through rankedCntrs and add values to otherCntrs
+            otherValues += rankedValues[i][1]; // We only want elements from the second column
+        }
+  
+        // Adjusting rankedCntrs to go into pie chart
+        rankedValues.splice(4,rankedValues.length-5); // Remove every element but the first five
+        rankedValues.unshift(['Name','Number']) // Add labels
+        rankedValues.push(['Other',otherValues]); // Add total of other countries
+    
+        var data = google.visualization.arrayToDataTable(rankedValues); // Make pie chart
+
+        var options = {
+            /* title: 'This is where your fellow students are from:', */
+        };
+    
+        var chart = new google.visualization.PieChart(document.getElementById(this.id)); // Reference to pie chart in .html file
+        chart.draw(data, options);   
+    }
+
+    drawBarChart() {
+        let valueArr = []
+
+        for (let i=0; i<localStorage.length; i++) {
+            if (localStorage.key(i) != "Users") {
+                var usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                valueArr.push(usr[this.name]);
+            }
+        }
+
+        var numberOfValue = {};
+
+        valueArr.forEach(function(n) {  // Creates a property for each country with its amount as the value
+            numberOfValue[n] = (numberOfValue[n] || 0) + 1; 
+        });
+
+        var rankedValues = []; // Create 2-dimensional array from object numberOfCountry
+        
+        for(let x in numberOfValue){
+            rankedValues.push([x, numberOfValue[x]])
+        }
+
+        rankedValues.sort(function(a,b){ // Sort array descending by the amount a country occurs
+            return b[1] - a[1];
+        });
+
+        rankedValues.unshift(['Name','Number']) // Add labels, as required by Google Charts
+
+        var data = google.visualization.arrayToDataTable(rankedValues); // Make pie chart
+
+        var options = {
+            /* title: 'This is where your fellow students are from:', */
+        };
+    
+        var chart = new google.visualization.BarChart(document.getElementById(this.id)); // Reference to bar chart in .html file
+        chart.draw(data, options);
+    }
+}
+
+var countries = new ChartData('nationality', 'countryChart');
+var genders = new ChartData('gender','genderChart');
+var districts = new ChartData('district','cphChart')
+var pizzaPasta = new ChartData('food','pizzaChart');
+var relStatus = new ChartData('status','statusChart');
+var beerWine = new ChartData('drinks','drinksChart');
+
+function drawCharts() {
+    countries.drawPieChart();
+    genders.drawPieChart();
+    districts.drawBarChart();
+    /* pizzaPasta.drawPieChart(); */
+    relStatus.drawPieChart();
+    /* beerWine.drawPieChart(); */
 }
